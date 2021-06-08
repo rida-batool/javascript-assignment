c28cb400a9edc5802299fff8154e08e23ab7af06
rida-batool <ridabatool.be13@gmail.com>
2021-06-09 01:09:14 +0500

    refactor

--- js/duplicate.js
+++ js/duplicate.js
@@ -1,32 +1,3 @@
-
-function evenOdd(givenArr) {
-    let evenNumbers = [];
-    let oddNumbers = [];
-    for (let index = 0; index < givenArr.length; index++){
-
-        let number = givenArr[index];
-        if (number % 2 === 0) {
-            evenNumbers.push(number);
-            
-        }
-        else {
-            oddNumbers.push(number);
-            
-        }
-        
-    }    
-    return [evenNumbers,oddNumbers];
-};
-
-// let result = evenOdd([1,2,3,4,5,6,7,8,9,10]);
-// console.log(result);
-
-// let result1 = evenOdd([23,24,25,26,27,28,29]);
-// console.log(result1);
-
-// let result2 = evenOdd([]);
-// console.log(result2);
-
 // let position = 0
 // let numbers = [0, 1, 2, 3, 30, 20, 10, 45, 100, 1, 25, 2300, 78, 1,2];
 // let a = numbers[position];
@@ -61,5 +32,4 @@ console.log(duplicateOutput);
 
 let duplicateOutput1 = getDuplicates([0,1,1,1,1,2,2,3,4,56,56,78,78]);
 console.log(duplicateOutput1);
-
 
---
+++ js/home.js
@@ -0,0 +1,30 @@
+
+function evenOdd(givenArr) {
+    let evenNumbers = [];
+    let oddNumbers = [];
+    for (let index = 0; index < givenArr.length; index++){
+
+        let number = givenArr[index];
+        if (number % 2 === 0) {
+            evenNumbers.push(number);
+            
+        }
+        else {
+            oddNumbers.push(number);
+            
+        }
+        
+    }    
+    return [evenNumbers,oddNumbers];
+};
+
+// let result = evenOdd([1,2,3,4,5,6,7,8,9,10]);
+// console.log(result);
+
+// let result1 = evenOdd([23,24,25,26,27,28,29]);
+// console.log(result1);
+
+// let result2 = evenOdd([]);
+// console.log(result2);
+
+
