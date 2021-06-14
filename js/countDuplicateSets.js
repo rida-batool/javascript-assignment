b26a200a99892b608e3b4d2e94361ddb06e97668
rida-batool <ridabatool.be13@gmail.com>
2021-06-14 21:00:51 +0500

    find duplicate using sets

---
+++ js/countDuplicateSets.js
@@ -0,0 +1,27 @@
+
+function countDuplicatesSets(array) {
+
+        let checkDuplicate = new Set();
+
+        for (var index = 0; index < array.length; index++){
+            var number = array[index];
+
+            if (checkDuplicate.has(number)){
+                 console.log('Duplicate number is ', number);
+            }
+            else {checkDuplicate.add(number)}
+
+}
+}
+
+console.log("--------------Start Find duplicate with sets-------------")
+duplicateOutput = countDuplicatesSets([0,1,1,1,2,2,2,3,4,56,78,78,100,100]);
+console.log(duplicateOutput);
+
+duplicateOutput1 = countDuplicatesSets([]);
+console.log(duplicateOutput1);
+
+
+console.log("--------------End Find duplicate with sets-------------")
+
+
